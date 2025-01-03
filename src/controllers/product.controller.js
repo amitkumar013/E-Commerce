242b27c417165c917443f1ee12a97782d2d6802c
amit013021 <amit013021@gmail.com>
2025-01-03 20:50:27 +0530

    add product model, and multer middleware

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../services/asyncHandler.js";
+import { ApiError } from "../services/ApiError.js";
+import {Product} from "../models/product.model.js";
+import { ApiResponse } from "../services/ApiResponse.js";
+
+//--------------------Create Product--------------------
+
+ 
