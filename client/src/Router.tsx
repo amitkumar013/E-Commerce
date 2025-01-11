1c03a5b764921b0376e08959bc51755bc48d8183
amit013021 <amit013021@gmail.com>
2025-01-11 22:08:48 +0530

    add AuthLayout and routing for login page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "./layout/DashboardLayout";
+import AuthLayout from "./layout/AuthLayout";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <DashboardLayout/>,
-    }
+    },
+
+    {
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+           
+        ],
+      },
 ])
 
 export default router;
