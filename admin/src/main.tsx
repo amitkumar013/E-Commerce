bd3125e815577b725a301c03c7e9ddc8aaea1664
amit013021 <amit013021@gmail.com>
2025-01-29 17:48:36 +0530

    add auth context

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
+import { AuthProvider } from './context/authContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <RouterProvider router={router} /> 
+    
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+       
   </StrictMode>,
 )
